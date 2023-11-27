139c9a38a0a75c8a4978fccd2bd1861faebce4c5
Emmaly <emmaly@emma.ly>
2023-11-26 17:21:36 -0800

    Updates and validation

--- custom_components/elevenlabs-tts/tts.py
+++ custom_components/elevenlabs-tts/tts.py
@@ -1,22 +1,38 @@
-import requests
-from homeassistant.components.tts import HomeAssistantTTSProvider
+import aiohttp
+import logging
+from homeassistant.components.tts import Provider
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 
-class ElevenLabsTTSProvider(HomeAssistantTTSProvider):
+_LOGGER = logging.getLogger(__name__)
+
+# Configuration Schema
+PLATFORM_SCHEMA = vol.Schema(
+    {
+        vol.Required("api_key"): cv.string,
+        vol.Optional("model_id"): cv.string,
+        vol.Optional("voice_id"): cv.string,
+        vol.Optional("stability"): vol.All(cv.positive_float, vol.Range(min=0.0, max=1.0)),
+        vol.Optional("similarity_boost"): vol.All(cv.positive_float, vol.Range(min=0.0, max=1.0)),
+        vol.Optional("style"): vol.All(cv.positive_float, vol.Range(min=0.0, max=1.0)),
+        vol.Optional("use_speaker_boost"): cv.boolean,
+    }
+)
+
+class ElevenLabsTTSProvider(Provider):
     def __init__(self, hass, conf):
         self.api_key = conf.get('api_key')
+        self.hass = hass
 
-    def get_tts_audio(self, message, language, options=None):
+    async def async_get_tts_audio(self, message, language, options=None):
         default_voice_id = "piTKgcLEGmPE4e6mEKli"
         voice_id = options.get('voice_id', default_voice_id) if options else default_voice_id
 
         default_model_id = "eleven_multilingual_v1"
         model_id = options.get('model_id', default_model_id) if options else default_model_id
 
-        # API URL with the voice ID
         url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
-
-        # Initialize voice settings only with provided options
-        voice_settings = {k: v for k, v in (options or {}).items() if k in ['similarity_boost', 'stability', 'style', 'use_speaker_boost']}
+        voice_settings = {k: v for k, v in (options or {}).items() if k in ['stability', 'similarity_boost', 'style', 'use_speaker_boost']}
 
         payload = {
             "text": message,
@@ -28,9 +44,14 @@ class ElevenLabsTTSProvider(HomeAssistantTTSProvider):
             "Content-Type": "application/json"
         }
 
-        response = requests.request("POST", url, json=payload, headers=headers)
-
-        if response.status_code == 200:
-            return "audio/mpeg", response.content
-        else:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=payload, headers=headers) as response:
+                    if response.status == 200:
+                        return "audio/mpeg", await response.read()
+                    else:
+                        _LOGGER.error(f"Error from ElevenLabs API: {response.status}")
+                        return None, None
+        except Exception as e:
+            _LOGGER.error(f"Error communicating with ElevenLabs API: {e}")
             return None, None
